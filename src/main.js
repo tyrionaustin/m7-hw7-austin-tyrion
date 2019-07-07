c5a940db9e25c99945ed3e60d4d0d09d0a3395e1
Tyrion Austin <tyrion.austin@ufl.edu>
2019-07-07 11:21:10 -0500

    Add javascript to hide and show message

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+const flavor = document.getElementById('hidden');
 let iceCream = document.getElementById('matcha');
   iceCream.addEventListener('mouseover', () => {
     iceCream.src = 'matcha2.jpg';
@@ -5,3 +6,16 @@ let iceCream = document.getElementById('matcha');
   iceCream.addEventListener('mouseout', () => {
     iceCream.src = 'matcha-ice-cream.jpg';
   }, false);
+
+
+
+
+const showMessage = () => {
+  hidden.classList.remove('hide')
+}
+const hideMessage = () => {
+  hidden.classList.add('hide')
+}
+
+iceCream.addEventListener('mouseover', showMessage);
+iceCream.addEventListener('mouseout', hideMessage);
